97fa21eed255fdbc35893eb37d368fef0e7b49c8
SuanChoi-Claire <clairechoi.h@gmail.com>
2025-04-08 20:13:15 +0900

    p124 수정삭제,CROS관련설정

--- src/main/java/org/zerock/mallapi/config/CustomServletConfig.java
+++ src/main/java/org/zerock/mallapi/config/CustomServletConfig.java
@@ -2,6 +2,7 @@ package org.zerock.mallapi.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.zerock.mallapi.controller.formatter.LocalDateFormatter;
 
@@ -14,4 +15,18 @@ public class CustomServletConfig implements WebMvcConfigurer {
     registry.addFormatter(new LocalDateFormatter());
  }
     
+
+public void addCorsMappings(CorsRegistry registry){
+
+
+   registry. addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("HEAD","GET","POST","PUT","DELETE","OPTIONS")
+            .maxAge(300)
+            .allowedHeaders("Authorization","Cache-Control","Content-type");
+}
+
+
+
+
 }
--- src/main/java/org/zerock/mallapi/controller/TodoController.java
+++ src/main/java/org/zerock/mallapi/controller/TodoController.java
@@ -1,7 +1,11 @@
 package org.zerock.mallapi.controller;
 
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.zerock.mallapi.dto.PageRequestDTO;
@@ -11,6 +15,8 @@ import org.zerock.mallapi.service.TodoService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.PostMapping;
+
 
 @RestController
 @RequiredArgsConstructor
@@ -33,5 +39,36 @@ public class TodoController {
         return service.list(pageRequestDTO);
     }
 
+
+   
+    @PostMapping("/")
+    public Map<String ,Long> register(@RequestBody TodoDTO todoDTO){
+        log.info("TodoDTO"+ todoDTO);
+        Long tno = service.register(todoDTO);
+        return Map.of("TNO",tno);
+    }
+
+    @PostMapping("/{tno}")
+    public Map<String, String> Modify(
+
+    @PathVariable(name="tno")Long tno,
+    @RequestBody TodoDTO todoDTO) {
+
+    todoDTO.setTno(tno);
+
+    log.info("Modify:"+ todoDTO);
+    service.modify(todoDTO);
+    return Map.of("RESULT", "SUCCESS");
+
+    }
+
+    @DeleteMapping("/{tno}")
+    public Map<String, String> remove (@PathVariable(name= "tno")Long tno){
+        log.info("Remove" +tno);
+        service.remove(tno);
+        return Map.of("Result","Success");
+    }
+
+    
 
 }
