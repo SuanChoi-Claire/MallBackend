c953912ad0afc78871961badc865f67dffc850d4
SuanChoi-Claire <clairechoi.h@gmail.com>
2025-05-16 16:35:05 +0900

    p198 엔티티처리하기

---
+++ src/main/java/org/zerock/mallapi/domain/Product.java
@@ -0,0 +1,67 @@
+package org.zerock.mallapi.domain;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+
+@Entity
+@Table(name = "tbl_product")
+@Getter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    private String pno;
+
+    private String pname;
+    
+    private int price;
+
+    private String pdesc;
+
+    private boolean delFlag;
+
+
+    @ElementCollection
+    @Builder.Default
+    private List<ProductImage> imageList = new ArrayList<>();
+
+    public void changePrice(int price) {
+        this.price = price;
+    }
+
+    public void changeDesc(String desc) {
+        this.pdesc = desc;
+    }
+
+    public void changeName(String name) {
+        this.pname = name;
+    }
+
+    public void addimage(ProductImage image) {
+        image.setOrd(this.imageList.size());
+        imageList.add(image);
+    }
+
+
+    public void addImageString(String fileName){
+
+        ProductImage productImage = ProductImage.builder()
+        .fileName(fileName)
+        .build();
+        addimage(productImage);
+
+    }
+
+    public void clearList(){
+        this.imageList.clear();
+    }
+}
