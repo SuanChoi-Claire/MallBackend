a368875be8e45a9af1f6ab7616a081eb3239441b
SuanChoi-Claire <clairechoi.h@gmail.com>
2025-05-16 12:12:28 +0900

    p194 특정이미지조회controller

--- src/main/java/org/zerock/mallapi/controller/ProductController.java
+++ src/main/java/org/zerock/mallapi/controller/ProductController.java
@@ -10,8 +10,15 @@ import org.zerock.mallapi.service.util.CustomFileUtil;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
@@ -35,6 +42,11 @@ public class ProductController {
 
     }
 
+
+    @GetMapping("/view/{fileName}")
+    public ResponseEntity<Resource> viewFileGET(@PathVariable String fileName){
+        return fileUtil.getFile(fileName);
+    }
 
     
     
