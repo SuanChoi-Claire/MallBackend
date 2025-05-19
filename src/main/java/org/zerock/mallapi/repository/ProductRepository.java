6de27f8b4ae256d99d3e4d19b0e1fa45a8a46844
SuanChoi-Claire <clairechoi.h@gmail.com>
2025-05-19 20:04:54 +0900

    p204 쿼리문 가져오기 이해못함,일단pass

--- src/main/java/org/zerock/mallapi/repository/ProductRepository.java
+++ src/main/java/org/zerock/mallapi/repository/ProductRepository.java
@@ -1,8 +1,16 @@
 package org.zerock.mallapi.repository;
 
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.zerock.mallapi.domain.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
     
+    @EntityGraph(attributePaths = "imageList")
+    @Query("select p from Product p where p.pno = :pno")
+    Optional<Product> selectOne(@Param("pno") Long pno);
 }
--- src/test/java/org/zerock/mallapi/repository/ProductRepositoryTests.java
+++ src/test/java/org/zerock/mallapi/repository/ProductRepositoryTests.java
@@ -1,5 +1,6 @@
 package org.zerock.mallapi.repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.junit.jupiter.api.Test;
@@ -7,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.zerock.mallapi.domain.Product;
 
+import jakarta.transaction.Transactional;
 import lombok.extern.log4j.Log4j2;
 
 @SpringBootTest
@@ -33,5 +35,33 @@ public class ProductRepositoryTests {
             log.info("---------------");
         }
 
+    }
+
+    @Transactional
+    @Test
+    public void testRead(){
+
+        Long pno = 1L;
+
+        Optional<Product> result = productRepository.findById(pno);
+
+        Product product = result.orElseThrow();
+
+        log.info(product + "///////////////////////");
+        log.info(product.getImageList());
+    }
+
+
+    @Test
+    public void testRead2(){
+
+        Long pno = 1L;
+
+        Optional<Product> result = productRepository.findById(pno);
+
+        Product product = result.orElseThrow();
+
+        log.info(product + "///////////////////////");
+        log.info(product.getImageList());
     }
 }
